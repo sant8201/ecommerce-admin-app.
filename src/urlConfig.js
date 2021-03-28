70e19671715dab2d5792e4f5c27a4950f4849df6
Jorge Luis dos Santos <71775223+sant8201@users.noreply.github.com>
2021-03-28 04:07:17 +0300

    Update urlConfig.js

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
-const baseUrl = process.env.API || "https://flipkart-rest-server.herokuapp.com";
+const baseUrl = process.env.API || "https://pragas-rest-server.herokuapp.com";
 //const baseUrl = "http://localhost:2000";
 
 export const api = `${baseUrl}/api`;
